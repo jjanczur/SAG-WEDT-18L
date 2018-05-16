02fc341f2158506e0958b75183e0b7058fa72b41
sgrzemski <aNdroiD175>
2018-05-16 12:43:07 +0200

    Created basic message handler in RestaurantResearcherAgent. Removed unused imports. Changed method names.

--- src/main/java/actors/menuResearcher/MenuResearcherAgent.java
+++ src/main/java/actors/menuResearcher/MenuResearcherAgent.java
@@ -1,20 +1,7 @@
 package actors.menuResearcher;
 import akka.actor.AbstractActor;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-
 
 public class MenuResearcherAgent extends AbstractActor {
-
-
     //methods
     @Override
     public Receive createReceive() {
--- src/main/java/actors/restaurantResearcher/RestaurantResearcherAgent.java
+++ src/main/java/actors/restaurantResearcher/RestaurantResearcherAgent.java
@@ -1,8 +1,8 @@
 package actors.restaurantResearcher;
 
-import actors.menuResearcher.Restaurant;
-import akka.actor.AbstractLoggingActor;
+import akka.actor.AbstractActor;
 import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -15,7 +15,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class RestaurantResearcherAgent extends AbstractLoggingActor {
+public class RestaurantResearcherAgent extends AbstractActor {
     //variables
     ArrayList restaurantsListJson;
     ArrayList restaurantsList;
@@ -26,18 +26,22 @@ public class RestaurantResearcherAgent extends AbstractLoggingActor {
     double latitude = 51.490489;
     double longtitude = -0.167910;
 
-    public void onReceive(Object message) {
+    @Override
+    public Receive createReceive() {
+        ReceiveBuilder rbuilder = ReceiveBuilder.create();
 
+        rbuilder.match(String.class, s -> {
+            System.out.println("Received message: " + s.toLowerCase());
+        });
+
+        return rbuilder.build();
     }
+
     public static Props props(){
         return Props.create(RestaurantResearcherAgent.class);
     }
-    @Override
-    public Receive createReceive() {
-        return null;
-    }
 
-    public void getRestaurantsData() {
+    private void getZomatoRestaurantsData() {
         String sURL = "https://developers.zomato.com/api/v2.1/search?entity_id="+cityId
                 +"&entity_type=city&q=lunch&count="+restaurantCount
                 +"&lat="+latitude+"&lon="+longtitude+"&radius="+radius+"&sort=rating&order=desc";
@@ -64,6 +68,40 @@ public class RestaurantResearcherAgent extends AbstractLoggingActor {
         Gson gJson = new Gson();
         restaurantsListJson = gJson.fromJson(jsonArr, ArrayList.class);
         restaurantsList = new ArrayList<Restaurant>();
+
+        for(int i=0;i<restaurantsListJson.size();i++){
+            restaurantsList.set(i,gJson.fromJson((String)restaurantsListJson.get(i), Restaurant.class));
+        }
+    }
+
+    private void getGoogleRestaurantsData() {
+        String sURL = "https://developers.zomato.com/api/v2.1/search?entity_id="+cityId
+                +"&entity_type=city&q=lunch&count="+restaurantCount
+                +"&lat="+latitude+"&lon="+longtitude+"&radius="+radius+"&sort=rating&order=desc";
+        String allRestaurantsJson = new String();
+
+        try {
+            URL url = new URL(sURL);
+            HttpURLConnection request = (HttpURLConnection)url.openConnection ();
+            request.setRequestMethod("GET");
+            request.setRequestProperty("user-key", apiUserKey);
+            request.connect();
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(request.getInputStream()));
+            allRestaurantsJson = in.readLine();
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = (JsonObject)jsonParser.parse(allRestaurantsJson);
+        /*todo: Make conversion from json list restaurant class directly */
+        JsonArray jsonArr = jsonObject.getAsJsonArray("restaurants");
+        Gson gJson = new Gson();
+        restaurantsListJson = gJson.fromJson(jsonArr, ArrayList.class);
+        restaurantsList = new ArrayList<Restaurant>();
+
         for(int i=0;i<restaurantsListJson.size();i++){
             restaurantsList.set(i,gJson.fromJson((String)restaurantsListJson.get(i), Restaurant.class));
         }
